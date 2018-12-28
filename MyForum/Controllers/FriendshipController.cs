79b4c9c40a66aff9142be25506b0633c3b144744
KaiHao <okh8609@gmail.com>
2018-12-28 18:58:12 +0800

    加入好友功能已完成

--- MyForum/Controllers/FriendshipController.cs
+++ MyForum/Controllers/FriendshipController.cs
@@ -21,37 +21,9 @@ namespace MyForum.Controllers
         }
         #endregion
 
-        #region 搜尋好友
-        [Authorize]
-        public ActionResult SearchList(string Search)
-        {
-            //宣告一個新頁面模型
-            FriendshipView Data = new FriendshipView();
-            //將傳入值Search(搜尋)放入頁面模型中
-            Data.Search = Search;
-            //從Service中取得頁面所需陣列資料
-            Data.DataList = fsService.GetSearchFriendList(Data.Search);
-            return PartialView(Data); //將頁面資料傳入View中
-        }
-        #endregion
-
-        #region 好友列表
-        [Authorize]
-        public ActionResult MyFriendList(string Account)
-        {
-            //宣告一個新頁面模型
-            FriendshipView Data = new FriendshipView();
-            //將傳入值Search(搜尋)放入頁面模型中
-            Data.Search = Account;
-            //從Service中取得頁面所需陣列資料
-            Data.DataList = fsService.GetSearchFriendList(Data.Search);
-            return PartialView(Data); //將頁面資料傳入View中
-        }
-        #endregion
-
         #region 真．好友列表
         [Authorize]
-        public ActionResult FriendList(string Search, string Account)
+        public ActionResult FriendList(string Search)
         {
             //宣告一個新頁面模型
             FriendshipView Data = new FriendshipView();
@@ -60,7 +32,7 @@ namespace MyForum.Controllers
             //從Service中取得頁面所需陣列資料
             if(String.IsNullOrEmpty(Data.Search))
             {
-                Data.DataList = fsService.GetMyFriendList(Account);
+                Data.DataList = fsService.GetMyFriendList(User.Identity.Name);
             }
             else
             {
@@ -69,7 +41,6 @@ namespace MyForum.Controllers
             return PartialView(Data); //將頁面資料傳入View中
         }
         #endregion
-
 
         #region 新增好友
         //新增好友一開始載入頁面
@@ -81,8 +52,8 @@ namespace MyForum.Controllers
 
         //新增好友傳入資料時的Action
         [Authorize] //設定此Action須登入
-        [HttpPost] //設定此Action只接受頁面POST資料傳入
-        public ActionResult Add(String p2)
+        [HttpGet] //設定此Action只接受頁面POST資料傳入
+        public ActionResult Add(string p2)
         {
             //使用Service來新增一筆資料
             fsService.Add(User.Identity.Name, p2);
