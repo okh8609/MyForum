74bde133eb7b29f962827f444e983dbb36c598fe
KaiHao <okh8609@gmail.com>
2018-12-30 17:35:41 +0800

    註冊葉面 修好了

--- MyForum/ViewModel/MemberRegisterView.cs
+++ MyForum/ViewModel/MemberRegisterView.cs
@@ -17,8 +17,8 @@ namespace MyForum.ViewModel
         public string Password { get; set; }
 
         [DisplayName("確認密碼")]
-        [Compare("Password", ErrorMessage = "兩次密碼輸入不一致")]
         [Required(ErrorMessage = "請輸入確認密碼")]
+        [Compare("Password", ErrorMessage = "兩次密碼輸入不一致")]
         public string PasswordCheck { get; set; }
     }
 }
